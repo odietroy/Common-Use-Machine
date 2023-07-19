81c79d5e94b8e4918c7cab24064120d66eb691ab
odietroy <139933140+odietroy@users.noreply.github.com>
2023-07-19 16:26:37 -0700

    Update maininterface.py

--- maininterface.py
+++ maininterface.py
@@ -3,5 +3,5 @@ import Calculator
 print("Hello and welcome to the Common Use Machine")
 option = input("Select option here --> ")
 if option.lower() == "calc":
-    Calculator
+    Calculator()
     print("pass")
